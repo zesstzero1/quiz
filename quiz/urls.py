d01964ccb0b684ebf2f7c0cd073c4fd916756147
zesstzero1 <s5701012630204@email.kmutnb.ac.th>
2018-03-29 01:17:16 -0700

    use base of polls app to create template and pattern of quesion now can see how many of voted number for each choice

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'quiz'
 urlpatterns = [
-    path('', views.index, name='index'),
+    # ex: /quiz/
+    path('', views.IndexView.as_view(), name='index'),
+    # ex: /quiz/5/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # ex: /pquiz/5/results/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # ex: /quiz/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
