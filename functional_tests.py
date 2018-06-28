f789aae60849ba0fbcafd994baa5838e47f3be03
zesstzero1 <s5701012630204@email.kmutnb.ac.th>
2018-06-27 21:39:08 -0700

    done functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -16,14 +16,24 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_calculator_and_use_functions(self):
         
         # Paul known a online quiz app.
-        self.browser.get('http://localhost:8000/quiz/')
-        # Paul notices the page title and header mention 'QUIZ'
-        self.assertIn('QUIZ', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('QUIZ', header_text)
+        self.browser.get('http://localhost:8000/quiz/1/')
+        # Paul notices the page title and header mention 'quiz'
+        self.assertIn('quiz', self.browser.title)
+        # Paul see question '50 Dollas more values than 10 Dollas?'
+        question = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('50 Dollas more values than 10 Dollas?', question)
         
-         
+        # Paul knew the answer then clicked on the choice 'True'
+        time.sleep(1)
+        choice = self.browser.find_element_by_xpath(".//input[@type='radio' and @value='1']").click()
+        time.sleep(1) 
         
+        # Paul accepted the answer 'True' by clicking the "Submit" button
+        submit = self.browser.find_element_by_xpath(".//input[@type='submit' and @value='Vote']").click()
+        time.sleep(3)
+
+
+
                 
         
 if __name__ == '__main__':
