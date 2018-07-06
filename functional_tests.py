77a649f5e81e87e5ef116994f70f375f6802b929
zesstzero1 <s5701012630204@email.kmutnb.ac.th>
2018-07-06 19:15:48 +0700

    finish functional_test and add gitignore for quiz app (1st time only add gitignore of project test29)

--- functional_tests.py
+++ functional_tests.py
@@ -16,7 +16,16 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_use_functions(self):
         
         # Paul known a online quiz app.
-        self.browser.get('http://localhost:8000/quiz/1/')
+        self.browser.get('http://localhost:8000')
+        # Paul notices the page title and header mention 'Quiz App'
+        self.assertIn('Quiz App', self.browser.title)
+        # Paul see 'Welcome to Quiz App'
+        question = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('Welcome to Quiz App', question)
+        # Paul see Link 'Click here to do the Question' and clicked it
+        h_link = self.browser.find_element_by_link_text('Click here to do the Question').click()
+        time.sleep(1)
+
         # Paul notices the page title and header mention 'quiz'
         self.assertIn('quiz', self.browser.title)
         # Paul see question '50 Dollas more values than 10 Dollas?'
@@ -30,9 +39,24 @@ class NewVisitorTest(unittest.TestCase):
         
         # Paul accepted the answer 'True' by clicking the "Submit" button
         submit = self.browser.find_element_by_xpath(".//input[@type='submit' and @value='Vote']").click()
-        time.sleep(3)
+        time.sleep(2)
 
+        # Paul notices the page title and header mention 'result'
+        self.assertIn('result', self.browser.title)
+        # Paul see question '50 Dollas more values than 10 Dollas?'
+        question = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('50 Dollas more values than 10 Dollas?', question)
 
+        # Paul see result of 'True' 
+        true = self.browser.find_element_by_id('1').text
+        self.assertIn('True', true)
+
+        # Paul see result of 'False'
+        false = self.browser.find_element_by_id('2').text
+        self.assertIn('False', false)
+        
+        # Paul do not want to do the question anymore then Paul close the browser
+        self.browser.quit()
 
                 
         
