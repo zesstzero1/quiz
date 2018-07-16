558000e8cd9ef8a83a5b5badac268ba6f7fdb837
zesstzero1 <s5701012630204@email.kmutnb.ac.th>
2018-07-16 21:38:54 +0700

    done functional_test

--- functional_tests.py
+++ functional_tests.py
@@ -20,32 +20,53 @@ class NewVisitorTest(unittest.TestCase):
         # Paul notices the page title and header mention 'Quiz App'
         self.assertIn('Quiz App', self.browser.title)
         # Paul see 'Welcome to Quiz App'
-        question = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('Welcome to Quiz App', question)
-        # Paul see Link 'Click here to do the Question' and clicked it
-        h_link = self.browser.find_element_by_link_text('Click here to do the Question').click()
+        header1 = self.browser.find_element_by_tag_name('h1').text  
+        self.assertIn('Welcome to Quiz App', header1)
+        # Paul see 'Add your Question'
+        header2 = self.browser.find_element_by_tag_name('h2').text  
+        self.assertIn('Add your Question', header2)
+
+        time.sleep(1)
+        # Paul add the question 'Is this a Quiz App?' to inputbox        
+        inputbox = self.browser.find_element_by_id('new_question')
+        inputbox.send_keys('Is this a Quiz App?')
+        time.sleep(1)
+        # Paul enter 'Add' button
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        # Paul see 'Question List' below
+        header3 = self.browser.find_element_by_tag_name('h3').text
+        self.assertIn('Question list', header3)
+
+        # Paul see his question that's he made 'Is this a Quiz App?' and clicked it
+        question_of_Paul = self.browser.find_element_by_link_text('Is this a Quiz App?').click()
+        time.sleep(2)
+        
         # Paul notices the page title and header mention 'quiz'
         self.assertIn('quiz', self.browser.title)
-        # Paul see question '50 Dollas more values than 10 Dollas?'
+        # Paul see question 'Is this a Quiz App?'
         question = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('50 Dollas more values than 10 Dollas?', question)
-        
+        self.assertIn('Is this a Quiz App?', question)
+
+        # Paul see 'Add True and False' for create choices and clicked it
+        create_choice = self.browser.find_element_by_link_text('Add True and False').click()
+        time.sleep(1)
+
         # Paul knew the answer then clicked on the choice 'True'
         time.sleep(1)
-        choice = self.browser.find_element_by_xpath(".//input[@type='radio' and @value='1']").click()
+        choice = self.browser.find_element_by_xpath(".//input[@type='radio' and @id='choice1']").click()
         time.sleep(1) 
         
         # Paul accepted the answer 'True' by clicking the "Submit" button
         submit = self.browser.find_element_by_xpath(".//input[@type='submit' and @value='Vote']").click()
-        time.sleep(2)
+        time.sleep(1)
 
         # Paul notices the page title and header mention 'result'
         self.assertIn('result', self.browser.title)
-        # Paul see question '50 Dollas more values than 10 Dollas?'
+        # Paul see question 'Is this a Quiz App?'
         question = self.browser.find_element_by_tag_name('h1').text  
-        self.assertIn('50 Dollas more values than 10 Dollas?', question)
+        self.assertIn('Is this a Quiz App?', question)
 
         # Paul see result of 'True' 
         true = self.browser.find_element_by_id('1').text
@@ -54,7 +75,19 @@ class NewVisitorTest(unittest.TestCase):
         # Paul see result of 'False'
         false = self.browser.find_element_by_id('2').text
         self.assertIn('False', false)
+        time.sleep(1)    
+    
+        # Paul done the quiz but he wants to delete his question 'Is this a Quiz App?'
+        # so he came back to detail page by clicking 'Vote again?' below
+        vote_again = self.browser.find_element_by_link_text('Vote again?').click()
+        time.sleep(1)
+
+        # at the bottom there is a 'delete this question'
+        # Paul clicked it to delete 'Is this a Quiz App'
+        del_q = self.browser.find_element_by_link_text('delete this question').click()
+        time.sleep(1)        
         
+        # browser bring Paul back to home page
         # Paul do not want to do the question anymore then Paul close the browser
         self.browser.quit()
 
